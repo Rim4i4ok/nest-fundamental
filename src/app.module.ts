f3da1d4002ac2c894afcb645a11be5b929083faa
Rim4i4ok <rim4i4ok@gmail.com>
2022-08-20 01:05:08 +0300

    added connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 // import { CoffeesController } from './coffees/coffees.controller';
 // import { CoffeesService } from './coffees/coffees.service';
 import { CoffeeModule } from './coffee/coffee.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeeModule],
+  imports: [
+    CoffeeModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // type of our database
+      host: 'localhost', // database host
+      port: 5432, // database host
+      username: 'postgres', // username
+      password: 'pass123', // user password
+      database: 'postgres', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
