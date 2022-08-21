87401021bcd3f3f99789d6b2bf1354ba351014a9
Rim4i4ok <rim4i4ok@gmail.com>
2022-08-21 21:10:02 +0300

    dynamic module

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 import { CoffeeRatingService } from './coffee-rating.service';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    DatabaseModule.regiser({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', // username
+      password: 'pass123', // user password
+    }),
+    CoffeesModule,
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
